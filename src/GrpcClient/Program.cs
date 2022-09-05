c1cdccac59b3fae6c821345f4ca82f6e1d7c26c7
mifmasterz <mifmasterz@yahoo.com>
2022-09-05 10:47:38 +0700

    add sample grpc

---
+++ src/GrpcClient/Program.cs
@@ -0,0 +1,15 @@
+﻿// See https://aka.ms/new-console-template for more information
+using Grpc.Net.Client;
+using GrpcContract;
+using ProtoBuf.Grpc.Client;
+
+Console.WriteLine("Hello, World!");
+using var channel = GrpcChannel.ForAddress("https://localhost:5001");
+var client = channel.CreateGrpcService<IGreeterService>();
+
+var reply = await client.SayHelloAsync(
+    new HelloRequest { Name = "GreeterClient" });
+
+Console.WriteLine($"Greeting: {reply.Message}");
+Console.WriteLine("Press any key to exit...");
+Console.ReadKey();
---
+++ src/GrpcServer/GreeterService.cs
@@ -0,0 +1,23 @@
+﻿using GrpcContract;
+using ProtoBuf.Grpc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GrpcServer
+{
+    public class GreeterService : IGreeterService
+    {
+        public Task<HelloReply> SayHelloAsync(HelloRequest request, CallContext context = default)
+        {
+            return Task.FromResult(
+                    new HelloReply
+                    {
+                        Message = $"Hello {request.Name}"
+                    });
+        }
+    }
+
+}
